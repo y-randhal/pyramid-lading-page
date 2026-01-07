93087d12757b34cd14c5c94735135c30ad36368f
y_randhal <yuu.rodrigues@gmail.com>
2026-01-07 06:51:10 -0300

    enable github-pages on project

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   distDir: process.env.NEXT_DIST_DIR || '.next',
-  output: process.env.NEXT_OUTPUT_MODE,
   experimental: {
     outputFileTracingRoot: path.join(__dirname, '../'),
   },
@@ -14,6 +13,11 @@ const nextConfig = {
     ignoreBuildErrors: false,
   },
   images: { unoptimized: true },
+  output: 'export', // Forces Next.js to build a static site
+  basePath: '/pyramid-lading-page',
+  images: {
+    unoptimized: true, // Required because GitHub Pages doesn't support the Next.js Image Optimizer
+  },
 };
 
 module.exports = nextConfig;
